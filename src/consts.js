8de89a582a23552aec242be38567a12537a231c2
LuisGilGB <luis.gilgb@gmail.com>
2019-10-23 17:48:50 +0200

    Class names preffixes fix

--- src/consts.js
+++ src/consts.js
@@ -1,75 +1,75 @@
-export const DEFAULT_CLASS_NAME = 'dlmmg-container';
-export const DEFAULT_CHILD_CLASS_NAME = 'dlmmg-container-item';
+export const DEFAULT_CLASS_NAME = 'lggb-react-container';
+export const DEFAULT_CHILD_CLASS_NAME = 'lggb-react-container-item';
 
 export const LAYOUTS_CFG = {
     auto: {
-        contClassName: 'dlmmg-container-auto',
-        childClassName: 'dlmmg-container-auto-item'
+        contClassName: 'lggb-react-container-auto',
+        childClassName: 'lggb-react-container-auto-item'
     },
     fit: {
-        contClassName: 'dlmmg-container-fit',
-        childClassName: 'dlmmg-container-fit-item'
+        contClassName: 'lggb-react-container-fit',
+        childClassName: 'lggb-react-container-fit-item'
     },
     center: {
-        contClassName: 'dlmmg-container-center',
-        childClassName: 'dlmmg-container-center-item'
+        contClassName: 'lggb-react-container-center',
+        childClassName: 'lggb-react-container-center-item'
     },
-    vflex: {
-        contClassName: 'dlmmg-container-vflex',
-        childClassName: 'dlmmg-container-vflex-item'
+    colflex: {
+        contClassName: 'lggb-react-container-colflex',
+        childClassName: 'lggb-react-container-colflex-item'
     },
-    hflex: {
-        contClassName: 'dlmmg-container-hflex',
-        childClassName: 'dlmmg-container-hflex-item'
+    rowflex: {
+        contClassName: 'lggb-react-container-rowflex',
+        childClassName: 'lggb-react-container-rowflex-item'
     }
 }
 
 export const FLEX_ALIGN_CLASS_NAMES = {
     start: {
-        contClassName: 'dlmng-container-flex-align-start',
-        childClassName: 'dlmng-container-flex-align-start-item'
+        contClassName: 'lggb-react-container-flex-align-start',
+        childClassName: 'lggb-react-container-flex-align-start-item'
     },
     end: {
-        contClassName: 'dlmng-container-flex-align-end',
-        childClassName: 'dlmng-container-flex-align-end-item'
+        contClassName: 'lggb-react-container-flex-align-end',
+        childClassName: 'lggb-react-container-flex-align-end-item'
     },
     center: {
-        contClassName: 'dlmng-container-flex-align-center',
-        childClassName: 'dlmng-container-flex-align-center-item'
+        contClassName: 'lggb-react-container-flex-align-center',
+        childClassName: 'lggb-react-container-flex-align-center-item'
     },
     stretch: {
-        contClassName: 'dlmng-container-flex-align-stretch',
-        childClassName: 'dlmng-container-flex-align-stretch-item'
+        contClassName: 'lggb-react-container-flex-align-stretch',
+        childClassName: 'lggb-react-container-flex-align-stretch-item'
     },
     baseline: {
-        contClassName: 'dlmng-container-flex-align-baseline',
-        childClassName: 'dlmng-container-flex-align-baseline-item'
+        contClassName: 'lggb-react-container-flex-align-baseline',
+        childClassName: 'lggb-react-container-flex-align-baseline-item'
     }
 }
 
 export const FLEX_JUSTIFY_CLASS_NAMES = {
     start: {
-        contClassName: 'dlmng-container-flex-justify-start',
-        childClassName: 'dlmng-container-flex-justify-start-item'
+        contClassName: 'lggb-react-container-flex-justify-start',
+        childClassName: 'lggb-react-container-flex-justify-start-item'
     },
     end: {
-        contClassName: 'dlmng-container-flex-justify-end',
-        childClassName: 'dlmng-container-flex-justify-end-item'
+        contClassName: 'lggb-react-container-flex-justify-end',
+        childClassName: 'lggb-react-container-flex-justify-end-item'
     },
     center: {
-        contClassName: 'dlmng-container-flex-justify-center',
-        childClassName: 'dlmng-container-flex-justify-center-item'
+        contClassName: 'lggb-react-container-flex-justify-center',
+        childClassName: 'lggb-react-container-flex-justify-center-item'
     },
     spaceBetween: {
-        contClassName: 'dlmng-container-flex-justify-spacebetween',
-        childClassName: 'dlmng-container-flex-justify-spacebetween-item'
+        contClassName: 'lggb-react-container-flex-justify-spacebetween',
+        childClassName: 'lggb-react-container-flex-justify-spacebetween-item'
     },
     spaceAround: {
-        contClassName: 'dlmng-container-flex-justify-spacearound',
-        childClassName: 'dlmng-container-flex-justify-spacearound-item'
+        contClassName: 'lggb-react-container-flex-justify-spacearound',
+        childClassName: 'lggb-react-container-flex-justify-spacearound-item'
     },
     spaceEvenly: {
-        contClassName: 'dlmng-container-flex-justify-spaceevenly',
-        childClassName: 'dlmng-container-flex-justify-spaceevenly-item'
+        contClassName: 'lggb-react-container-flex-justify-spaceevenly',
+        childClassName: 'lggb-react-container-flex-justify-spaceevenly-item'
     }
 }
