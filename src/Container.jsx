bcb00ca1de9cac4692488910aa4640f262e105f1
LuisGilGB <luis.gilgb@gmail.com>
2019-10-23 18:00:00 +0200

    Created Container.jsx

---
+++ src/Container.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import {isArray, isObject} from '@luisgilgb/js-utils';
+import {classNamer} from '@luisgilgb/react-utils';
+import {
+    getFlexAlignClassName,
+    getFlexJustifyClassName,
+    getLayoutClassName,
+    getLayoutContainerClassName,
+    getLayoutChildClassName,
+    getCmpClassName,
+    getContainerClassName,
+    getChildClassName,
+    mapChildClassName,
+    mapChild
+} from './utils';
+
+const Container = props => {
+    const {
+        children,
+        className,
+        layout,
+        width,
+        height,
+        flex: flexGrow,
+        minWidth,
+        maxWidth,
+        minHeight,
+        maxHeight,
+        onClick,
+        ...otherProps
+    } = props;
+
+    const style = {
+        width,
+        height,
+        flexGrow,
+        minWidth,
+        maxWidth,
+        minHeight,
+        maxHeight
+    }
+
+    return (
+        <div
+            className={getContainerClassName(layout, className)}
+            onClick={onClick}
+            {...otherProps}
+            style={style}
+        >
+            {isArray(children) ?
+                children.map(child => mapChild(child, mapChildClassName(layout))) :
+                (isObject(children === 'object') ? mapChild(children, mapChildClassName(layout)) : children)
+            }
+        </div>
+    );
+}
+
+export default Container;
