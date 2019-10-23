ac284f25ff34862d46a6d9bbb938ee355c218f78
LuisGilGB <luis.gilgb@gmail.com>
2019-10-23 17:36:01 +0200

    Created consts file for class names and more

---
+++ src/consts.js
@@ -0,0 +1,75 @@
+export const DEFAULT_CLASS_NAME = 'dlmmg-container';
+export const DEFAULT_CHILD_CLASS_NAME = 'dlmmg-container-item';
+
+export const LAYOUTS_CFG = {
+    auto: {
+        contClassName: 'dlmmg-container-auto',
+        childClassName: 'dlmmg-container-auto-item'
+    },
+    fit: {
+        contClassName: 'dlmmg-container-fit',
+        childClassName: 'dlmmg-container-fit-item'
+    },
+    center: {
+        contClassName: 'dlmmg-container-center',
+        childClassName: 'dlmmg-container-center-item'
+    },
+    vflex: {
+        contClassName: 'dlmmg-container-vflex',
+        childClassName: 'dlmmg-container-vflex-item'
+    },
+    hflex: {
+        contClassName: 'dlmmg-container-hflex',
+        childClassName: 'dlmmg-container-hflex-item'
+    }
+}
+
+export const FLEX_ALIGN_CLASS_NAMES = {
+    start: {
+        contClassName: 'dlmng-container-flex-align-start',
+        childClassName: 'dlmng-container-flex-align-start-item'
+    },
+    end: {
+        contClassName: 'dlmng-container-flex-align-end',
+        childClassName: 'dlmng-container-flex-align-end-item'
+    },
+    center: {
+        contClassName: 'dlmng-container-flex-align-center',
+        childClassName: 'dlmng-container-flex-align-center-item'
+    },
+    stretch: {
+        contClassName: 'dlmng-container-flex-align-stretch',
+        childClassName: 'dlmng-container-flex-align-stretch-item'
+    },
+    baseline: {
+        contClassName: 'dlmng-container-flex-align-baseline',
+        childClassName: 'dlmng-container-flex-align-baseline-item'
+    }
+}
+
+export const FLEX_JUSTIFY_CLASS_NAMES = {
+    start: {
+        contClassName: 'dlmng-container-flex-justify-start',
+        childClassName: 'dlmng-container-flex-justify-start-item'
+    },
+    end: {
+        contClassName: 'dlmng-container-flex-justify-end',
+        childClassName: 'dlmng-container-flex-justify-end-item'
+    },
+    center: {
+        contClassName: 'dlmng-container-flex-justify-center',
+        childClassName: 'dlmng-container-flex-justify-center-item'
+    },
+    spaceBetween: {
+        contClassName: 'dlmng-container-flex-justify-spacebetween',
+        childClassName: 'dlmng-container-flex-justify-spacebetween-item'
+    },
+    spaceAround: {
+        contClassName: 'dlmng-container-flex-justify-spacearound',
+        childClassName: 'dlmng-container-flex-justify-spacearound-item'
+    },
+    spaceEvenly: {
+        contClassName: 'dlmng-container-flex-justify-spaceevenly',
+        childClassName: 'dlmng-container-flex-justify-spaceevenly-item'
+    }
+}
