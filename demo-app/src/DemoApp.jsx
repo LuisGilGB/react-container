1330ce47dcf3f2867764279185e58a581e2a34de
Luis Gil Gutiérrez de la Barreda <luis.gilgb@gmail.com>
2020-03-20 15:03:19 +0100

    Added code to demo app

--- demo-app/src/DemoApp.jsx
+++ demo-app/src/DemoApp.jsx
@@ -18,8 +18,37 @@ function DemoApp() {
           Learn React
         </a>
       </header>
-      <Container>
-        This is into a Container
+      <Container
+        style={{
+          backgroundColor: 'papayawhip',
+          padding: '15px'
+        }}
+      >
+        <p>This is a Container with no layout</p>
+        <Container
+          layout="hflex"
+          style={{
+            padding: '15px',
+            backgroundColor: 'mediumseagreen'
+          }}
+        >
+          <Container
+            flex={2}
+            style={{
+              border: '3px solid steelblue'
+            }}
+          >
+            This is a container with flex 2 into an hflex Container.
+          </Container>
+          <Container
+            flex={1}
+            style={{
+              border: '3px solid steelblue'
+            }}
+          >
+            This is a container with flex 1 into an hflex Container.
+          </Container>
+        </Container>
       </Container>
     </div>
   );
